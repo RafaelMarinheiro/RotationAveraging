defb62916a6aee7b0ef2e4ef825de644a168b1b7
bae43 <bae43@cornell.edu>
2014-12-08 21:20:00 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='rotation_averaging',
+      version='1.0',
+      description='Rotation Averaging Library',
+      author='Rafael Marinheiro, Bryce Evans',
+      author_email='rf356@cornell.edu, bae43@cornell.edu',
+      py_modules=['algorithms, minimization'],
+      )
